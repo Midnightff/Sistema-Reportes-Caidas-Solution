d442c92cadcf61375c113402cb2d84516c982c7c
Midnight <alanalvarez19r@gmail.com>
2024-11-14 12:58:39 -0600

    Agregar archivos de proyecto.

---
+++ Sistema-Reportes-Caidas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Reportes_Caidas.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
